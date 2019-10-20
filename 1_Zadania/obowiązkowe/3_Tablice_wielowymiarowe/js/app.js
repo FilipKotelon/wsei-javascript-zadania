cd7e37893a7c8c4fc5fa313fd40247463aaee2db
Kotelon Filip <filip.kotelon@microsoft.wsei.edu.pl>
2019-10-20 15:38:14 +0200

    Zadania z tablic 2D

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -13,3 +13,36 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+// function checkArray(arr){
+//     let endArr = [];
+
+//     for(let i=0; i<arr.length; i++){
+//         let check = true;
+
+//         for(let j=0; j<arr[i].length; j++){
+
+//             if(arr[i][j] % 2 !== 0){
+//                 check = false;
+//                 break;
+//             }
+
+//         }
+
+//         endArr.push(check);
+//     }
+
+//     return endArr;
+// }
+
+// checkArray([
+//     [11, 12],
+//     [42, 2],
+//     [-4, -120],
+//     [0, 0],
+//     [1, 34],
+// ]);
+
+console.log(task1Array[3][2]);
+console.log(task1Array[1].length);
+console.log(task1Array[3][2]);
