79a5dd63f8bacc2be358bd8b1eee38a5d99569cb
Kotelon Filip <filip.kotelon@microsoft.wsei.edu.pl>
2019-12-08 15:53:10 +0100

    Zadania z rozdziału 9

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -14,35 +14,90 @@ var task2Array = [
     [9, 10, 11, 12]
 ];
 
-// function checkArray(arr){
-//     let endArr = [];
+// Zadanie 0
+function checkArray(arr){
+    let endArr = [];
 
-//     for(let i=0; i<arr.length; i++){
-//         let check = true;
+    for(let i=0; i<arr.length; i++){
+        let check = true;
 
-//         for(let j=0; j<arr[i].length; j++){
+        for(let j=0; j<arr[i].length; j++){
 
-//             if(arr[i][j] % 2 !== 0){
-//                 check = false;
-//                 break;
-//             }
+            if(arr[i][j] % 2 !== 0){
+                check = false;
+                break;
+            }
 
-//         }
+        }
 
-//         endArr.push(check);
-//     }
+        endArr.push(check);
+    }
 
-//     return endArr;
-// }
+    return endArr;
+}
 
-// checkArray([
-//     [11, 12],
-//     [42, 2],
-//     [-4, -120],
-//     [0, 0],
-//     [1, 34],
-// ]);
+checkArray([
+    [11, 12],
+    [42, 2],
+    [-4, -120],
+    [0, 0],
+    [1, 34],
+]);
 
+// Zadanie 1
 console.log(task1Array[3][2]);
 console.log(task1Array[1].length);
 console.log(task1Array[3][2]);
+
+//Zadanie 2
+//1.
+for(let i = 0; i < task2Array.length; i++){
+    console.log(task2Array[i]);
+}
+//2.
+for(let i = 0; i < task2Array.length; i++){
+    console.log(task2Array[i].length);
+}
+//3.
+for(let i = 0; i < task2Array.length; i++){
+    for(let j = 0; j < task2Array[i].length; j++){
+        console.log(task2Array[i][j]);
+    }
+}
+
+//Zadanie 3
+function print2DArray(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            console.log(arr[i][j]);
+        }
+    }
+}
+
+//Zadanie 4
+let testArr = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4],
+    [1, 2, 3],
+    [1, 2],
+    [1]
+]
+
+print2DArray(testArr);
+
+//Zadanie 4
+function create2DArray(rows, columns){
+    let endArr = [];
+    let counter = 1;
+
+    for(let i = 0; i < rows; i++){
+        let insideArr = [];
+        for(let j = 0; j < columns; j++){
+            insideArr.push(counter);
+            counter++;
+        }
+        endArr.push(insideArr);
+    }
+
+    return endArr;
+}
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,66 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    //Zadanie 0
+    const getDatasInfo = (elements) => {
+        const arr = [];
+
+        for(let i = 0; i < elements.length; i++){
+            arr.push(elements[i].dataset.color);
+        }
+
+        return arr;
+    }
+
+    console.log(getDatasInfo(links));
+
+    //Zadanie 1
+    console.log(homeElement);
+    console.log(childElements);
+    console.log(banner);
+    console.log(blocks);
+    console.log(links);
+
+    for(let i = 0; i < childElements.length; i++){
+        console.log('tag: ' + childElements[i].tagName, 'klasa: ' + childElements[i].className);
+    }
+
+    for(let i = 0; i < blocks.length; i++){
+        console.log('tag: ' + blocks[i].tagName, 'klasa: ' + blocks[i].className);
+    }
+
+    for(let i = 0; i < links.length; i++){
+        console.log('tag: ' + links[i].tagName, 'klasa: ' + links[i].className);
+    }
+
+    //Zadanie 2
+    for(let i = 0; i < blocks.length; i++){
+        //Wyświetla wewnętrzny kod HTML elementu
+        console.log(blocks[i].innerHTML);
+        //Wyświetla cały kod HTML elementu
+        console.log(blocks[i].outerHTML);
+        //Zmienia wewnętrzny kod HTML elementu
+        blocks[i].innerHTML = 'Nowa wartość diva o klasie blocks';
+    }
+
+    //Zadanie 3
+    const mainFooter = document.querySelector('#mainFooter');
+    const getId = (element) => {
+        console.log('id: ' + element.id);
+    }
+
+    console.log(getId(mainFooter));
+
+    //Zadanie 4
+    const getTags = (elements) => {
+        const arr = [];
+
+        for(let i = 0; i < elements.length; i++){
+            arr.push(elements[i].tagName);
+        }
+
+        return arr;
+    }
+
+    console.log(getTags(childElements));
 });
